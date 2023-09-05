1d0cdf909157ec9b82c57d56fe4556721b8ef6a8
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-09-05 14:20:10 -0300

    main atualizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//rotas
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
