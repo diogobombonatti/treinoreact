4beb07c5f4fb566303dadcfd823a948fe6989515
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-09-05 13:55:10 -0300

    main atualizado apagado imports antigos

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
