f18c5c6cfe764fc49606a31d8e3dcf4f9c3e6fef
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-09-10 19:59:09 -0300

    URLS lista imagens

--- src/components/listaProdutos.js
+++ src/components/listaProdutos.js
@@ -1,29 +1,32 @@
 export const listaProdutos = [
     {
         id: 1,
-        nome: 'Iphone 8 ',
-        preco: 4000
+        nome: 'Iphone 8',
+        preco: 4000,
+        imagem: 'https://cdn.awsli.com.br/600x450/296/296387/produto/27823722/3e6e7b5ea9.jpg'
     },
     {
         id: 2,
         nome: 'Iphone X',
-        preco: 6000
+        preco: 6000,
+        imagem: 'https://microimport.com.br/storage/placa-mae-original-iphone-x-de-64gb-silver-prata.png'
     },
     {
         id: 3,
-        nome: "Xiaomi Mi10",
-        preco: 950
+        nome: 'Xiaomi Mi10',
+        preco: 950,
+        imagem: 'https://br.celulares.com/fotos/xiaomi-mi-10-ultra-87240-g-alt.jpg'
     },
-
     {
         id: 4,
-        nome: "Tablet Samsung",
-        preco: 200
-
+        nome: 'Tablet Samsung',
+        preco: 200,
+        imagem: 'https://images.samsung.com/is/image/samsung/p6pim/br/sm-x205nzauzto/gallery/br-galaxy-a8-sm-x205-sm-x205nzauzto-530991473?$650_519_PNG$' 
     },
     {
         id: 5,
-        nome: "Ipad",
-        preco: 120
+        nome: 'Ipad',
+        preco: 120,
+        imagem: 'https://m.media-amazon.com/images/I/61sEJ2+OAbL._AC_SX342_SY445_.jpg' 
     }
-]
+    ];
--- src/routes/EditarProdutos/index.jsx
+++ src/routes/EditarProdutos/index.jsx
@@ -4,7 +4,7 @@ import { listaProdutos } from "../../components/listaProdutos";
 export default function DetalhesProduto() {
   const { id } = useParams();
 
-  const produto = listaProdutos.find((prod) => prod.id === id);
+  const produto = listaProdutos.find((prod) => prod.id === parseInt(id));
 
   if (!produto) {
     return <div>Produto não encontrado.</div>;
@@ -15,7 +15,7 @@ export default function DetalhesProduto() {
       <h1>Detalhes do Produto</h1>
       <h2>{produto.nome}</h2>
       <p>Preço: R$ {produto.preco}</p>
-      <img src={`url_da_foto_do_produto`} alt={produto.nome} />
+      <img src={produto.imagem} alt={produto.nome} />
     </main>
   );
 }
