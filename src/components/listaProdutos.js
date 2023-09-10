d3c33f50377b003978c856c715bde70ef2b77f51
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-09-10 19:03:20 -0300

    produtos adicionados na lista e 1 imagem promocao na home

--- src/components/listaProdutos.js
+++ src/components/listaProdutos.js
@@ -1,18 +1,29 @@
 export const listaProdutos = [
     {
-        id:1,
-        nome:'Teclado',
-        preco:150
+        id: 1,
+        nome: 'Teclado',
+        preco: 150
     },
     {
-        id:2,
-        nome:'Mouse',
-        preco:120
+        id: 2,
+        nome: 'Mouse',
+        preco: 120
     },
     {
-        id:3,
-        nome:"Monitor",
-        preco:950
+        id: 3,
+        nome: "Monitor",
+        preco: 950
     },
 
+    {
+        id: 4,
+        nome: "Headset",
+        preco: 200
+
+    },
+    {
+        id: 5,
+        nome: "Webcam",
+        preco: 120
+    }
 ]
--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -3,6 +3,9 @@ export default function Home() {
     return (
         <main>
             <h1>HOME</h1>
+            <img src="https://1.bp.blogspot.com/-vY35B15RvHE/YMuAOxVhRTI/AAAAAAAAuY4/e6rQ7v_yhwgvG3ouxzlVWSzOhXw9kTrxACNcBGAsYHQ/s300/promocao-2-am-desconto-notebook-mouse-gamer.jpg" alt="Compre junto com notebook" />
+        
+        
         </main>
 
     )
