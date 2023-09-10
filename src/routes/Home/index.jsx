f0dbae13b1c5cbbb6177254542613353fc325af7
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-09-10 19:19:54 -0300

    nova tentativa colocar imagens lado a lado pelo css

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,19 +1,10 @@
 export default function Home() {
 
-    const imgStyle = {
-        display: 'inline-block',
-        marginRight: '10px', 
-    };
-
-
-
-
-
     return (
         <main>
             <h1>HOME</h1>
-            <img src="https://1.bp.blogspot.com/-vY35B15RvHE/YMuAOxVhRTI/AAAAAAAAuY4/e6rQ7v_yhwgvG3ouxzlVWSzOhXw9kTrxACNcBGAsYHQ/s300/promocao-2-am-desconto-notebook-mouse-gamer.jpg" alt="Compre junto com notebook" style ={imgStyle} />
-            <img src="https://scontent.fcgh9-1.fna.fbcdn.net/v/t1.6435-9/206825880_3761883060583088_3278516047727026837_n.jpg?stp=cp0_dst-jpg_e15_p320x320_q65&_nc_cat=102&ccb=1-7&_nc_sid=9e2e56&_nc_ohc=Mu6cDR2uu3sAX8NPXCN&_nc_oc=AQkS75-HZEa6pVOTDQ4MTnx6o095KijqdxfXZSTaG7Vrs_DycHWp3SM4kzJXHlZ-Pa0feUlvX-e1-esb72THv4c7&_nc_ht=scontent.fcgh9-1.fna&oh=00_AfDXOLSPJQSQeXm5sSpQnQCcsm_ilBvDygYEhwPmWg285w&oe=6525B264" alt="Promoção Mouse Multilaser" style ={imgStyle} />
+            <img className="imagem-promocao" src="https://1.bp.blogspot.com/-vY35B15RvHE/YMuAOxVhRTI/AAAAAAAAuY4/e6rQ7v_yhwgvG3ouxzlVWSzOhXw9kTrxACNcBGAsYHQ/s300/promocao-2-am-desconto-notebook-mouse-gamer.jpg" alt="Compre junto com notebook"/>
+            <img className="imagem-promocao" src="https://scontent.fcgh9-1.fna.fbcdn.net/v/t1.6435-9/206825880_3761883060583088_3278516047727026837_n.jpg?stp=cp0_dst-jpg_e15_p320x320_q65&_nc_cat=102&ccb=1-7&_nc_sid=9e2e56&_nc_ohc=Mu6cDR2uu3sAX8NPXCN&_nc_oc=AQkS75-HZEa6pVOTDQ4MTnx6o095KijqdxfXZSTaG7Vrs_DycHWp3SM4kzJXHlZ-Pa0feUlvX-e1-esb72THv4c7&_nc_ht=scontent.fcgh9-1.fna&oh=00_AfDXOLSPJQSQeXm5sSpQnQCcsm_ilBvDygYEhwPmWg285w&oe=6525B264" alt="Promoção Mouse Multilaser"/>
         
         </main>
 
