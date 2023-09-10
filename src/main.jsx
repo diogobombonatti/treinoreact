ffb5125dd90ea495e35263c0fdb2f66087fe366b
Diogo Bombonatti <126741260+diogobombonatti@users.noreply.github.com>
2023-09-10 20:02:17 -0300

    caminhos arrumados

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
-import EditarProduto from './routes/EditarProdutos/index.jsx'
+import DetalhesProduto from './routes/DetalhesProdutos/index.jsx'
 
 const router = createBrowserRouter([
   {
